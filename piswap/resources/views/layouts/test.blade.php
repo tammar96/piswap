d805fb7eca0924bb32b045a0ea088bd9ec6861e0
alakatos <alakatos@redhat.com>
2020-04-13 13:56:34 +0200

    [add] Rentals list/add/delete

--- piswap/resources/views/layouts/test.blade.php
+++ piswap/resources/views/layouts/test.blade.php
@@ -52,7 +52,7 @@
         </ul>
         <ul class="nav nav-pills flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="/rentals">Rentals</a>
+            <a class="nav-link" href="/borrows">Rentals</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" href="/profile">Profile</a>
@@ -78,7 +78,7 @@
         </ul>
         <ul class="nav nav-pills flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="/rentals">Rentals</a>
+            <a class="nav-link" href="/borrows">Rentals</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" href="/profile">Profile</a>
@@ -87,7 +87,7 @@
         @elseif (Auth::user()->hasRole('user'))
         <ul class="nav nav-pills flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="/rentals">My Rentals</a>
+            <a class="nav-link" href="/borrows">My Rentals</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" href="/profile">Profile</a>
