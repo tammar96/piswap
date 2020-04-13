364360a81759c0c47aad4ec5ee75b27e92d53c54
Marek Tamaskovic <tamaskovic.marek@gmail.com>
2020-04-13 13:20:00 +0200

    [b] syntax error

--- piswap/resources/views/layouts/test.blade.php
+++ piswap/resources/views/layouts/test.blade.php
@@ -49,7 +49,7 @@
         </ul>
         <ul class="nav nav-pills flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="/rentals">My Rentals</a>
+            <a class="nav-link" href="/rentals">Rentals</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" href="/profile">Profile</a>
@@ -72,7 +72,7 @@
         </ul>
         <ul class="nav nav-pills flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="/rentals">My Rentals</a>
+            <a class="nav-link" href="/rentals">Rentals</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" href="/profile">Profile</a>
